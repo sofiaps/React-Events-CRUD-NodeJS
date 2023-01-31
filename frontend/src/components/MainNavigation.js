bf84662bcc76f1aa402a43cda9e9898a09edf11d
sofiaps <31549943+sofiaps@users.noreply.github.com>
2023-01-31 09:15:32 +0100

    Add files via upload

---
+++ frontend/src/components/MainNavigation.js
@@ -0,0 +1,70 @@
+import { Form, NavLink, useRouteLoaderData } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup";
+
+function MainNavigation() {
+  const token = useRouteLoaderData("root");
+
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/events"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Events
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/newsletter"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Newsletter
+            </NavLink>
+          </li>
+          {!token && (
+            <li>
+              <NavLink
+                to="/auth?mode=login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Authentication
+              </NavLink>
+            </li>
+          )}
+        </ul>
+      </nav>
+      <div className={classes.rightContainer}>
+      <NewsletterSignup />
+      {token && (
+        <Form action="/logout" method="post">
+          <button>Logout</button>
+        </Form>
+      )}
+      </div>
+    </header>
+  );
+}
+
+export default MainNavigation;
